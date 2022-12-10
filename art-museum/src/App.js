5d715ff0c074fd527f8e8dca881992fe72a250b1
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-09 16:52:14 -0800

    Did some of pahse 1

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,10 +1,15 @@
 import harvardArt from './data/harvardArt'
 import GalleryNavigation from './components/GalleryNavigation/GalleryNavigation';
+import { Route } from 'react-router';
+import GalleryView from './components/GalleryView/GalleryView';
 
 function App() {
   return (
     <>
     <GalleryNavigation galleries={harvardArt.records} />
+    <Route path={'/galleries/:galleryId'}> 
+        <GalleryView data={harvardArt.records} />
+    </Route>
     </>
   );
 }
---
+++ art-museum/src/components/GalleryView/GalleryView.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router";
+
+const GalleryView = (record) => {
+    const {galleryId} = useParams();
+    return (
+        <header></header>
+    )
+}
+
+export default GalleryView;
